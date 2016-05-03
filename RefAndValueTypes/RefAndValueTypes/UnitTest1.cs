af1349294d9545e1dc9c8d00be32f2d82298de1c
DavidNemeth <david.nemeth90@gmail.com>
2016-05-03 14:47:46 +0200

    Identity test

---
+++ RefAndValueTypes/RefAndValueTypes/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefAndValueTypes
+{
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IdentityTest()
+        {
+            Invoice firstInvoice = new Invoice();
+            firstInvoice.ID = 1;
+            firstInvoice.Description = "Test";
+            firstInvoice.Amount = 0.0M;
+
+            Invoice secondInvoice = new Invoice();
+            secondInvoice.ID = 1;
+            secondInvoice.Description = "Test";
+            secondInvoice.Amount = 0.0M;
+
+            Assert.IsFalse(object.ReferenceEquals(secondInvoice, firstInvoice));
+            Assert.IsTrue(firstInvoice.ID == 1);
+
+            secondInvoice.ID = 2;
+
+            Assert.IsTrue(firstInvoice.ID == 1);
+            Assert.IsTrue(secondInvoice.ID == 2);
+
+            secondInvoice = firstInvoice;
+            Assert.IsTrue(object.ReferenceEquals(secondInvoice, firstInvoice));
+
+            secondInvoice.ID = 5;
+            Assert.IsTrue(firstInvoice.ID == 5);
+
+            firstInvoice.Description = "test2";
+            Assert.IsTrue(secondInvoice.Description == "test2");
+        }        
+    }
+}
