29315b0fdb4328699125ad9f668624d94e6fbb10
DavidNemeth <david.nemeth90@gmail.com>
2016-05-04 17:08:47 +0200

    Constructor Demo

---
+++ ConstructorDemo/ConstructorDemo/constructorDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo
+{
+    public class Bus
+    {
+        public static int busNo = 0;
+        public static string _name;
+
+        static Bus()
+        {
+            Console.WriteLine("Woey, it's a new day! Drivers are starting to work.");
+        }
+
+        public Bus(string name)
+        {            
+            busNo++;
+            _name = name;
+            Console.WriteLine("Bus #{0} goes from the depot. Driver name: {1}", busNo, _name);
+        }
+
+        public Bus()
+            : this("Anonimous")
+        { 
+        }
+             
+    }
+
+    class constructorDemo
+    {
+        static void Main(string[] args)
+        {
+            Bus busOne = new Bus();
+            Bus busTwo = new Bus("Dave");            
+            Console.ReadLine();
+        }
+    }
+}
