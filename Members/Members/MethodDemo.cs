54d4a6541e0b242884ba6bb53cab3b991d333468
DavidNemeth <david.nemeth90@gmail.com>
2016-05-04 14:55:58 +0200

    MethodDemo

---
+++ Members/Members/MethodDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Members
+{
+    public class MethodDemo
+    {
+        public MethodDemo()
+        {
+            WriteAsBytes(32, 42, 255, Int16.MaxValue, Int32.MaxValue);
+            WriteAsBytes(new int[] { 2, 4, 5, 7, 9 });                    
+        }
+
+        private void WriteAsBytes(params int[] values)
+        {
+            foreach (var value in values)
+            {
+                byte[] bytes = BitConverter.GetBytes(value);
+                foreach (byte b in bytes)
+                {
+                    string message = String.Format("0x{0:X2} ", b);
+                    Debug.Write(message);
+                }
+
+                Debug.WriteLine("");
+            }
+        }
+    }
+}
