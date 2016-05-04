8633ce909b77831c8ff59b1811af7536702f5f56
DavidNemeth <david.nemeth90@gmail.com>
2016-05-04 15:17:23 +0200

    Overloading

--- Members/Members/MethodDemo.cs
+++ Members/Members/MethodDemo.cs
@@ -9,14 +9,30 @@ namespace Members
 {
     public class MethodDemo
     {
-        public MethodDemo()
+        public  MethodDemo()
         {
             WriteAsBytes(32, 42, 255, Int16.MaxValue, Int32.MaxValue);
-            WriteAsBytes(new int[] { 2, 4, 5, 7, 9 });                    
+            WriteAsBytes(new double[] { 2.0, 4.4, 5.2, 7.1, 9.2 });                    
         }
 
         private void WriteAsBytes(params int[] values)
         {
+            Debug.WriteLine("writing integers");
+            foreach (var value in values)
+            {
+                byte[] bytes = BitConverter.GetBytes(value);
+                foreach (byte b in bytes)
+                {
+                    string message = String.Format("0x{0:X2} ", b);
+                    Debug.Write(message);
+                }
+
+                Debug.WriteLine("");
+            }
+        }
+        private void WriteAsBytes(params double[] values)
+        {
+            Debug.WriteLine("writing doubles");
             foreach (var value in values)
             {
                 byte[] bytes = BitConverter.GetBytes(value);
