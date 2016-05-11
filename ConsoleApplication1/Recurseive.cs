5c7b9071020b7706c2afaaa21d655ab1c6552c0e
DavidNemeth <david.nemeth90@gmail.com>
2016-05-11 03:31:33 +0200

    Num and Char count

---
+++ ConsoleApplication1/Recurseive.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphAndNumTest
+{
+    class Recurseive
+    {
+        class Counting
+        {
+            static int counter = 0;
+            public int id;            
+            public char c;
+            public Counting()
+            {
+                id = counter;
+                c = (char)(((int)'a') + counter);
+                counter++;
+            }
+        }
+        static void Recurse(int i)
+        {
+            var c1 = new Counting();            
+            Console.Write("{0}\t{1}\n" ,c1.c,c1.id);  
+            if (i == 0)
+            {
+                return;
+            }
+            Recurse(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Recurse(100);
+            Console.Read();
+        }        
+    }
+}
