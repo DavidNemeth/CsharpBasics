46b80b3bf72001083eecae4e34a9eee1170e097e
DavidNemeth <david.nemeth90@gmail.com>
2016-05-18 11:03:00 +0200

    closuresDemo

--- DelegatesAndEvents/DelegatesAndEvents/DelegateDemo.cs
+++ DelegatesAndEvents/DelegatesAndEvents/DelegateDemo.cs
@@ -5,12 +5,10 @@ using System.Collections.Generic;
 namespace DelegatesAndEvents
 {
     class DelegateDemo
-    {
-        //delegate bool Mydeli(int n);
-        //delegate void ChainDeli();
-        //delegate T ReturnDeli<T>();
+    {        
         static void Main()
         {
+            #region delegateParameter
             int[] numbers = new int[] { 11, 3, 15, 6, 5, 2, 20, 8 };
             IEnumerable<int> result = GetNumbers(numbers, n => n < 10);
             foreach (var number in result)
@@ -18,28 +16,37 @@ namespace DelegatesAndEvents
                 Console.WriteLine(number);
             }
             Console.ReadLine();
+            #endregion
 
-            //Delegate chaining
-
+            #region DelegateChaining
             Action a = Foo;
             a += Goo;
             a += Moo;
             a();
             Console.ReadLine();
+            #endregion
 
+            #region funcAndAction
             //Func  -> delegate, can have return argument
             //Action -> delegate returns void
-
             Func<int> d = ReturnFive;
             d += ReturnTen;
             d += ReturnZero;
-                        
+
             foreach (int item in GetAllReturns(d))
                 Console.WriteLine(item);
-            
             Console.ReadLine();
+            #endregion
+
+            #region Closure
+            //c will reference different action than b thus making them unique
+            Action c = GiveMeAction();
+            Action b = GiveMeAction();
+            b(); c(); c(); c(); b(); c(); //b:i=4 j=9 c:i=8 j=13
+            GiveMeAction(); // i=0 j=5
+            #endregion
         }
-        #region mydeli
+        #region DelegateParameter
         static IEnumerable<int> GetNumbers(IEnumerable<int> numbers, Func<int, bool> deli)
         {
             foreach (int number in numbers)
@@ -50,13 +57,15 @@ namespace DelegatesAndEvents
                 }
             }
         }
+        #endregion
 
+        #region DelegateChaining
         static void Foo() { Console.WriteLine("Foo()"); }
         static void Goo() { Console.WriteLine("Goo()"); }
         static void Moo() { Console.WriteLine("Moo()"); }
         #endregion
 
-        #region returndeli
+        #region FuncAndAction
         static IEnumerable<T> GetAllReturns<T>(Func<T> d)
         {            
             foreach (Func<T> del in d.GetInvocationList())
@@ -65,6 +74,43 @@ namespace DelegatesAndEvents
         static int ReturnFive() { return 5; }
         static int ReturnTen() { return 10; }
         static int ReturnZero() { return 0; }
-        #endregion  
+        #endregion
+
+        #region closures
+        static Action GiveMeAction()
+        {
+            Action ret = null;
+
+            int i = 0;
+            int j = 5;
+            ret += () => i++;
+            ret += () => j++;
+            ret += () => { i++; j++; };
+            return ret;
+            //var temp = new ThisisWhatActuallyHappens();
+            //ret += temp.lambdais;
+            //ret += temp.anameless;
+            //ret += temp.function;
+            //return ret;
+        }
+
+        class ThisisWhatActuallyHappens
+        {
+            int i = 0;
+            int j = 5;
+            public void lambdais()
+            {
+                i++;
+            } 
+            public void anameless()
+            {
+                j++;
+            }
+            public void function()
+            {
+                i++;j++;
+            }
+        }
+        #endregion
     }
 }
