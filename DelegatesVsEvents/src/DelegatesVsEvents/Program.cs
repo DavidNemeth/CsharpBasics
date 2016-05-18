bffc1be5e60e1203f43d13b6403507b4db7fcad4
DavidNemeth <david.nemeth90@gmail.com>
2016-05-18 11:30:30 +0200

    Initial

---
+++ DelegatesVsEvents/src/DelegatesVsEvents/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DelegatesVsEvents
+{
+    public class Program
+    {       
+        public static void Main(string[] args)
+        {
+            TrainSignal trainSignal = new TrainSignal();
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+            trainSignal.HereComesATrain();
+            Console.WriteLine();
+            trainSignal.GoodToGo();
+        }
+    }
+}
---
+++ DelegatesVsEvents/src/DelegatesVsEvents/TrainSignal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace DelegatesVsEvents
+{
+    class TrainSignal
+    {
+        public Action TrainsAreComing; //void delegate reference
+        public Action GoodToGo;
+
+        public void HereComesATrain()
+        {
+            //logix
+            TrainsAreComing();
+        }
+        public void TrainIsGone()
+        {
+            //more logix
+            GoodToGo();
+        }
+    }
+
+    class Car
+    {
+        public Car(TrainSignal trainSignal)
+        {
+            trainSignal.TrainsAreComing += StopTheCar; //add to list(subscribe)
+            trainSignal.GoodToGo += MoveOn;
+        }
+
+        private void MoveOn()
+        {
+            Console.WriteLine("Good to go!");
+        }
+
+        private void StopTheCar()
+        {
+            Console.WriteLine("STAPHH!");
+        }
+    }
+}
