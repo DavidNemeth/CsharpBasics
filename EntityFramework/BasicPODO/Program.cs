950b5e77a47cec798e31cbe271c80904c078927f
DavidNemeth <david.nemeth90@gmail.com>
2016-05-25 08:39:55 +0200

    podo initial
    basic podo class

---
+++ EntityFramework/BasicPODO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace BasicPODO
+{
+    class Video
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    class VidContext : DbContext
+    {
+        public VidContext() : base(@"Data Source =(localdb)\ProjectsV13; Initial catalog = MyTestDb; Integrated Security = True") { }
+        public DbSet<Video> Videos { get; set; }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            var video = new Video
+            {
+                Title = "Warcraft",
+                Description = "Some kind of move with orcs n trolls"
+            };
+            var vidContext = new VidContext();
+            vidContext.Videos.Add(video);
+            vidContext.SaveChanges();
+        }
+    }
+}
