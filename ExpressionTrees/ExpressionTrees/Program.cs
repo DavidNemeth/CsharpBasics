e77f740946b56e6d05a52adc00ee30ac0db7e5d0
DavidNemeth <david.nemeth90@gmail.com>
2016-05-28 23:02:21 +0200

    initial

---
+++ ExpressionTrees/ExpressionTrees/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace ExpressionTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, bool>> test = i => i > 5;
+            
+            ConstantExpression constExp = Expression.Constant(5, typeof(int));
+            //Console.WriteLine(constExp.NodeType);
+            //Console.WriteLine(constExp.Type);
+            //Console.WriteLine(constExp.Value);
+
+            ParameterExpression iParam = Expression.Parameter(typeof(int), "i");
+            //Console.WriteLine(iParam.NodeType);
+            //Console.WriteLine(iParam.Type);
+            //Console.WriteLine(iParam.Name);
+
+            BinaryExpression greaterThan = Expression.GreaterThan(iParam, constExp);
+            Console.WriteLine(greaterThan.NodeType);
+            Console.WriteLine(greaterThan.Type);
+            Console.WriteLine(greaterThan.Left);
+            Console.WriteLine(greaterThan.Right);
+        }
+    }
+}
