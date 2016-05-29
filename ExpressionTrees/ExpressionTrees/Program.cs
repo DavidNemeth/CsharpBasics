1e89c249b41137f0d8c730dd5213e2f4c3aa9e94
DavidNemeth <david.nemeth90@gmail.com>
2016-05-29 11:32:49 +0200

    finished tree

--- ExpressionTrees/ExpressionTrees/Program.cs
+++ ExpressionTrees/ExpressionTrees/Program.cs
@@ -14,17 +14,21 @@ namespace ExpressionTrees
             //Console.WriteLine(constExp.NodeType);
             //Console.WriteLine(constExp.Type);
             //Console.WriteLine(constExp.Value);
-
             ParameterExpression iParam = Expression.Parameter(typeof(int), "i");
             //Console.WriteLine(iParam.NodeType);
             //Console.WriteLine(iParam.Type);
             //Console.WriteLine(iParam.Name);
-
             BinaryExpression greaterThan = Expression.GreaterThan(iParam, constExp);
-            Console.WriteLine(greaterThan.NodeType);
-            Console.WriteLine(greaterThan.Type);
-            Console.WriteLine(greaterThan.Left);
-            Console.WriteLine(greaterThan.Right);
+            //Console.WriteLine(greaterThan.NodeType);
+            //Console.WriteLine(greaterThan.Type);
+            //Console.WriteLine(greaterThan.Left);
+            //Console.WriteLine(greaterThan.Right);
+            Expression<Func<int, bool>> test2 =
+                Expression.Lambda<Func<int, bool>>(greaterThan, iParam);
+
+            Func<int, bool> myDelegate = test.Compile();
+            Console.WriteLine(myDelegate(3));
+            Console.WriteLine(myDelegate(8));
         }
     }
 }
