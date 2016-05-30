75e2f6786c00dc656b8d7e2fccb5c6715dcf640f
DavidNemeth <david.nemeth90@gmail.com>
2016-05-30 13:04:41 +0200

    Sync example one

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread1 = new Thread(SyncExampleOne.IncrementCount);
+            var thread2 = new Thread(SyncExampleOne.IncrementCount);
+            thread1.Start();
+            Thread.Sleep(500);
+            thread2.Start();
+            //for (int i = 0; i < Environment.ProcessorCount; i++)
+            //{
+            //    var thread = new Thread(DifferentMethod);                
+            //    thread.Start(i);
+            //}    
+        }
+        //static void DifferentMethod(object threadID)
+        //{
+        //    while (true)
+        //    {
+        //        Console.WriteLine("Hello from DifferentMethod "+ Thread.CurrentThread.ManagedThreadId);
+        //    }
+        //}
+    }
+
+    class SyncExampleOne
+    {
+        static int count = 0;
+        static object bot = new object();
+        public static void IncrementCount()
+        {
+            while (true)
+            {
+                lock (bot)
+                {
+                    int temp = count;
+                    Thread.Sleep(1000);
+                    count = temp + 1;
+                    Console.WriteLine("Thread ID " + Thread.CurrentThread.ManagedThreadId +
+                        " incremented count to " + count);
+                }                
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
